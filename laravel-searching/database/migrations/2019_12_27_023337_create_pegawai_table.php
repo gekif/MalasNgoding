2cb5447916e6b65dd32b95c3672bb7a2c32f09f1
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-27 09:42:27 +0700

    Setup Database Migrations

---
+++ laravel-searching/database/migrations/2019_12_27_023337_create_pegawai_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePegawaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->integer('pegawai_id')->primary();
+            $table->string('pegawai_nama');
+            $table->string('pegawai_jabatan');
+            $table->integer('pegawai_umur');
+            $table->text('pegawai_alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+}
